008d74fced94ced702dfbf53831a3d7c8426589e
2020113388@dgu.ac.kr <1079163@naver.com>
2022-09-21 13:54:10 +0900

    test.md를 추가하였습니다.

---
+++ IdeaProjects/HelloJava/src/ch01/pro1.java
@@ -0,0 +1,11 @@
+package ch01;
+
+public class pro1 {
+    public static void main(String[] args){
+        boolean isBoomuk;
+        isBoomuk = true;
+
+        System.out.println("당신은 부먹파입니까?");
+        System.out.println(isBoomuk);
+    }
+}
